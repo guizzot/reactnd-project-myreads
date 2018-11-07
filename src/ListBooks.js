a54a001a197d2dc1212894e4f0bbaa3eec680942
Guilherme Ribeiro <43709413+guizzot@users.noreply.github.com>
2018-11-07 01:29:43 -0200

    Refactor - Stateless Functional
    REFACTOR 
    - If class don't have any state to change, we can change to a stateless function.
    - Removed props.type from BooksFiltered
    - Now the books are filtered and passed to an array before being linked to the type

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,69 +1,73 @@
-import React, { Component } from 'react'
+import React from 'react'
 import BooksFiltered from './BooksFiltered'
 import { Link } from 'react-router-dom'
 
-class ListBooks extends Component {
+const ListBooks = props => {
 
-	render() {
+    const { books, onMoveShelf, filterBookType, handleMove } = props
+	const titleCurrentlyReading = 'Currently Reading'
+	const titleRead = 'Read'
+	const titleWantToRead = 'Want to Read'
+	const booksCurrentlyReading  = filterBookType(books,'currentlyReading')
+	const booksRead = filterBookType(books,'read')
+	const booksWantToRead = filterBookType(books,'wantToRead')
 
-		const { books, onMoveShelf } = this.props
-
-		return (
-			<div className="list-books">
+    return (
+    	<div className="list-books">
             	<div className="list-books-title">
               		<h1>MyReads</h1>
-              		<button> <Link
-              						className="a-button-search"
-              						to="/search"
-						     >Search</Link>
-					</button>
             	</div>
 				<div className="list-books-content">
 	              <div>
 	                <div className="bookshelf">
-	                  <h2 className="bookshelf-title">Currently Reading</h2>
+	                  <h2 className="bookshelf-title">{titleCurrentlyReading}</h2>
 	                    <div className="bookshelf-books">
 	                      <ol className="books-grid">
 					          <BooksFiltered
-				                            books={books}
+				                            books={booksCurrentlyReading}
 				                            onMoveShelf={onMoveShelf}
-				                            type='currentlyReading'
+				                            handleMove={handleMove}
 					          />
 						    </ol>
 	                    </div>
 	                </div>
 	                <div className="bookshelf">
-	                  <h2 className="bookshelf-title">Read</h2>
+	                  <h2 className="bookshelf-title">{titleRead}</h2>
 	                    <div className="bookshelf-books">
 	                      <ol className="books-grid">
 					          <BooksFiltered
-				                            books={books}
+				                            books={booksRead}
 				                            onMoveShelf={onMoveShelf}
-				                            type='read'
+				                            handleMove={handleMove}
 					          />
 						    </ol>
 	                    </div>
 	                </div>
 	                <div className="bookshelf">
-	                  <h2 className="bookshelf-title">Want to Read</h2>
+	                  <h2 className="bookshelf-title">{titleWantToRead}</h2>
 	                    <div className="bookshelf-books">
 	                      <ol className="books-grid">
 					          <BooksFiltered
-				                            books={books}
+				                            books={booksWantToRead}
 				                            onMoveShelf={onMoveShelf}
-				                            type='wantToRead'
+				                            handleMove={handleMove}
 					          />
 						    </ol>
 	                    </div>
 	                </div>
 	                <div className="open-search">
-	                  <a>Add a book</a>
-	                </div>
+	                	<Link
+      						className="a-button-search"
+      						to="/search"
+						>Add a Book</Link>
+					</div>
 	              </div>
 	          </div>
           </div>
-		)
-	}
+
+    )
+
+
 }
 
 export default ListBooks
